4f64fc72ec58c35432c45cbcd299b66e502bf3d8
Hai Nguyen <hhainguyen@gmail.com>
2013-11-14 00:17:39 +0000

    1. Add options to configure mapping service by editing config file.(hebridean.ldcms.inc ,geo_handler,update_editor) 2. Add mapping settings section to LDCMS settings in admin panel. (curios_admin) 3. Move editor_notes to "special" fields (like published and owned) (entities, views_default, ldcms,update,update_editor)

--- js/curios-geo-handler.js
+++ js/curios-geo-handler.js
@@ -16,39 +16,105 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software. If not, see <http://www.gnu.org/licenses/>.
  */
-var osMap, dragControl, marker, map2Marker;
-
+var osMap, dragControl, marker, mapMarker;
 (function($) {
     Drupal.behaviors.field_geo = {
         attach: function(context, settings) {
+	// set name for coordinate fields (for example geo:lat, hc:northing...). Check the configuration file under options/map_settings
+	  var 	x = settings.curiosMap.mapXName, 
+    	      	y = settings.curiosMap.mapYName,
+	      	defaultZoom = settings.curiosMap.defaultZoom,
+		defaultN = settings.curiosMap.defaultN,
+		defaultE = settings.curiosMap.defaultE;
             // switching between widgets
-            $('input[name=hc_northing],input[name=hc_easting]').live("focus", function(event) {
+            $('input[name='+y+'],input[name='+x+']').live("focus", function(event) {
 		// if already loaded, don't reload;
 		if ($('#map').attr("loaded")) return;
-
 		$('#map').attr("loaded",true);
 		$('#map').css("width", "100%");
 		$('#map').css("height", "500px"); 
-		var E = $("input[name=old_hc_easting]").val(), N = $("input[name=old_hc_northing]").val();
-		if ((E=="")||isNaN(E)) E = 141314;
-		if ((N=="")||isNaN(N)) N = 917410;
+		var E = $("input[name=old_"+x+"]").val(), N = $("input[name=old_"+y+"]").val();
+		if ((E=="")||isNaN(E)) E = defaultE;
+		if ((N=="")||isNaN(N)) N = defaultN;
 		var initialCenterPoint = new OpenSpace.MapPoint(E,N);
 		createMap(initialCenterPoint);
                 osMap.createMarker(initialCenterPoint);
 		createDragControl();
                 osMap.addControl(dragControl);
                 activateDragControl();
-		updateMarkerPositionToTextfield(initialCenterPoint);
+		if (E!=defaultE || N!=defaultN) 
+			updateMarkerPositionToTextfield(initialCenterPoint);
             });
-	   $('input[name=hc_northing],input[name=hc_easting]').live('blur', function(event) {
-		var E = $("input[name=hc_easting]").val(), N = $("input[name=hc_northing]").val();
-		if ((E=="")||isNaN(E)) E = 141314;
-		if ((N=="")||isNaN(N)) N = 917410;
+            $('input[name='+y+'],input[name='+x+']').live("blur", function(event) {
+		// update the marker based on textfield values
+		var E = $("input[name="+x+"]").val(), N = $("input[name="+y+"]").val();
+		if ((E=="")||isNaN(E)) E = defaultE;
+		if ((N=="")||isNaN(N)) N = defaultN;
 		var newCenterPoint = new OpenSpace.MapPoint(E,N);
 		osMap.getMarkerLayer().clearMarkers();
 		osMap.createMarker(newCenterPoint);
 		osMap.setCenter(newCenterPoint);
 	   });
+
+        // creates the hand map centred on the initialCenterPoint
+
+	function createMap(initialCenterPoint) {
+	  
+		osMap = new OpenSpace.Map("map");
+		osMap.setCenter(initialCenterPoint, 3);
+	}
+
+
+	// updates the display text to the marker location
+
+	function updateMarkerPositionToTextfield(lonlat) {
+	  
+		document.getElementsByName(x)[0].value = Math.round(lonlat.lon);
+		document.getElementsByName(y)[0].value = Math.round(lonlat.lat);
+	}
+
+	// creates the drag control for the map
+
+	function createDragControl() {
+	  
+
+	// Set dragging active on the default maker layer on map
+
+		dragControl = new OpenSpace.Control.DragMarkers(osMap.getMarkerLayer(),{
+
+	// onDrag create a lonlat variable with the lonlat (easting/northing in this projection) of the marker
+
+			onDrag: function( marker ){
+				var lonlat = marker.lonlat;
+				updateMarkerPositionToTextfield(lonlat);
+			},
+
+	// onComplete centre boths maps on the marker lonlat
+
+			onComplete: function( marker ){
+				var lonlat = marker.lonlat;
+		                osMap.setCenter(lonlat);
+			}
+		});
+	}
+
+	// Create the marker on the left hand map with the initial centre point
+
+
+	// Centre both maps on the lonlat passed through
+
+
+	function updateMapMarkerPosition(lonlat){
+		osMap.getMarkerLayer().clearMarkers();
+		mapMarker = osMap.createMarker(lonlat);
+		osMap.setCenter(lonlat);
+	}
+	// Activate the dragcontrol
+
+	function activateDragControl() {
+		dragControl.activate();  
+		osMap.getMarkerLayer().setDragMode(true);
+	}
         
             
         }
@@ -59,62 +125,4 @@ var osMap, dragControl, marker, map2Marker;
 
 
 
-// creates the hand map centred on the initialCenterPoint
 
-function createMap(initialCenterPoint) {
-  
-	osMap = new OpenSpace.Map("map");
-	osMap.setCenter(initialCenterPoint, 3);
-}
-
-
-// updates the display text to the marker location
-
-function updateMarkerPositionToTextfield(lonlat) {
-  
-	document.getElementsByName('hc_easting')[0].value = Math.round(lonlat.lon);
-	document.getElementsByName('hc_northing')[0].value = Math.round(lonlat.lat);
-}
-
-// creates the drag control for the map
-
-function createDragControl() {
-  
-
-// Set dragging active on the default maker layer on map
-
-	dragControl = new OpenSpace.Control.DragMarkers(osMap.getMarkerLayer(),{
-
-// onDrag create a lonlat variable with the lonlat (easting/northing in this projection) of the marker
-
-		onDrag: function( marker ){
-			var lonlat = marker.lonlat;
-			updateMarkerPositionToTextfield(lonlat);
-		},
-
-// onComplete centre boths maps on the marker lonlat
-
-		onComplete: function( marker ){
-			var lonlat = marker.lonlat;
-                        osMap.setCenter(lonlat);
-		}
-	});
-}
-
-// Create the marker on the left hand map with the initial centre point
-
-
-// Centre both maps on the lonlat passed through
-
-
-function updateMapMarkerPosition(lonlat){
-	osMap.getMarkerLayer().clearMarkers();
-	map2Marker = osMap.createMarker(lonlat);
-	osMap.setCenter(lonlat);
-}
-// Activate the dragcontrol
-
-function activateDragControl() {
-	dragControl.activate();  
-	osMap.getMarkerLayer().setDragMode(true);
-}
